9e4d3f8de610a69b8054a3ac5c095f2c80124065
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-02-04 20:11:16 +0530

    Add files via upload
    first commit server infinite loop

--- Server.java
+++ Server.java
@@ -1,22 +1,35 @@
-package networking;
+package networkingserveronfiniteloop;
 
+import java.io.DataOutputStream;
 import java.io.DataInputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 public static void main(String[] args) {
-	try {
-		ServerSocket ssoc = new ServerSocket(6000);
-		System.out.println("Server is ready to accept the request");
-		Socket ss = ssoc.accept();
-		System.out.println("Accept the request");
-		InputStream is = ss.getInputStream();
-		DataInputStream dis = new DataInputStream(is);
+	try
+	{
+	ServerSocket ssoc = new ServerSocket(6000);
+	System.out.println("Server is ready to accept the requests");
+	Socket ss = ssoc.accept();
+	System.out.println("Accept the request");
+	InputStream is = ss.getInputStream();
+	DataInputStream  dis = new DataInputStream(is);
+	OutputStream os = ss.getOutputStream();
+	DataOutputStream dos = new DataOutputStream(os);
+	Scanner scan  = new Scanner(System.in);
+	while(true)
+	{
 		String msg = dis.readUTF();
 		System.out.println(msg);
-	}
+		System.out.println("Enter a reply");
+		String reply = scan.nextLine();
+		dos.writeUTF(reply);
+		
+	}	}
 	catch(Exception e)
 	{
 		e.printStackTrace();
